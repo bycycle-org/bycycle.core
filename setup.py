e36621cc0991895fd2d6363376932aaae7f32c6f
Wyatt Lee Baldwin <wyatt.lee.baldwin@gmail.com>
2012-11-08 22:06:20 -0800

    Update setup.py.
    - Remove copyright/license headers (seems unnecessary?)
    - Change version from 0.5 to 0.5.dev0 (zest.releaser style)
    - Clarify GPL version is 3
    - Specify Python version 2.7
    - Update all dependency versions to latest (doesn't seem to cause any major
      issues when testing)
    - Clear out some other cruft

--- setup.py
+++ setup.py
@@ -1,52 +1,34 @@
-###############################################################################
-# $Id$
-# Created 2006-09-07
-#
-# Project setup for byCycle Core.
-#
-# Copyright (C) 2006, 2007 Wyatt Baldwin, byCycle.org <wyatt@bycycle.org>.
-# All rights reserved.
-#
-# For terms of use and warranty details, please see the LICENSE file included
-# in the top level of this distribution. This software is provided AS IS with
-# NO WARRANTY OF ANY KIND.
-###############################################################################
 from setuptools import setup, find_packages
 
 
 setup(
     name='bycycle.core',
-    version='0.5',
-    description='byCycle Core Services',
-    long_description='byCycle model, routing, and GIS related services.',
-    license='GNU General Public License (GPL)',
-    author='Wyatt L Baldwin, byCycle.org',
+    version='0.5.dev0',
+    description='byCycle core model and services',
+    long_description='byCycle model, routing, and geocoding.',
+    license='GPLv3',
+    author='Wyatt Baldwin, byCycle.org',
     author_email='wyatt@bycycle.org',
-    keywords='bicycle bike cycyle trip planner route finder',
+    keywords='bicycle bike cycle trip planner geocoder',
     url='http://bycycle.org/',
-    download_url='http://guest:guest@code.bycycle.org/Core/trunk#egg=byCycleCore-dev',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License (GPL)',
-        'Natural Language :: English',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python',
-        'Topic :: Education',
-        'Topic :: Scientific/Engineering :: GIS',
-        ],
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 2.7',
+    ],
     packages=find_packages(),
     zip_safe=False,
     install_requires=(
-        'Shapely>=1.0.11',
-        'pyproj>=1.8.5',
+        'Shapely>=1.2.16',
+        'pyproj>=1.9.2',
         'GeoJSON>=1.0.1',
-        'psycopg2>=2.0.11',
-        'SQLAlchemy>=0.5.5',
+        'psycopg2>=2.4.5',
+        'SQLAlchemy>=0.7.9',
         'Dijkstar>=1.0',
-        'nose>=0.11.1',
-        'simplejson>=2.0.9',
-        'Restler==dev,>=0.3dev-r92',
+        'nose>=1.2.1',
+        'simplejson>=2.6.2',
+        'Restler>=0.7.1',
     ),
 )
