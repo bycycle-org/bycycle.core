494785b364e6a138fd2fcc471c424934696a1ea1
Wyatt Lee Baldwin <wyatt.lee.baldwin@gmail.com>
2012-11-14 22:10:38 -0800

    Convert integrate script into proper console script.
    New name for script is bycycle-integrate.
    
    - Add [console_script] entry point.
    - Convert script to use argparse instead of optparse (much nicer!).
    - Clean up and correct module docstring, which is also used as the script's
      help text.
    - Remove crufty module header.

--- bycycle/core/scripts/integrate.py
+++ bycycle/core/scripts/integrate.py
@@ -1,186 +1,104 @@
-#!/usr/bin/env python
-###############################################################################
-# $Id: shp2pgsql.py 187 2006-08-16 01:26:11Z bycycle $
-# Created 2007-05-08.
-#
-# Data integration front end script.
-#
-# Copyright (C) 2006-2008 Wyatt Baldwin, byCycle.org <wyatt@bycycle.org>.
-# All rights reserved.
-#
-# For terms of use and warranty details, please see the LICENSE file included
-# in the top level of this distribution. This software is provided AS IS with
-# NO WARRANTY OF ANY KIND.
-###############################################################################
-"""
-Usage::
-
-    shp2pgsql.py [various options, see below]
+"""Import regional data into database.
 
 Example::
 
-    shp2pgsql.py --region portlandor --source pirate --layer str06oct -n
+    bycycle-integrate \
+        --region portlandor \
+        --source pirate \
+        --layer str06oct \
+        --no-prompt
 
-    In this example, --region is the region "key" matching the region's
-    Python module name and ``slug`` in the public.regions database table.
+    --region is the region key matching the region's Python module name and
+    slug in the public.regions database table.
 
-    --source is the name of a directory in ${HOME}/byCycleData/portlandor. In
-    this case, on my machine, it refers to the directory
-    /home/bycycle/byCycleData/portlandor/pirate.
+    --source is the name of a directory in ${HOME}/byCycleData/portlandor.
 
-    [Note that, for now, ${HOME}/byCycleData can't be changed from the command
-    line. It can be changed by creating an Integrator and setting its
-    ``base_data_path`` attribute. It might also be possible to set --source as
-    an absolute path, but I haven't tried this.]
+    For now, ${HOME}/byCycleData can't be changed from the command line. It
+    can be changed by creating a `model.data.Integrator` subclass and
+    setting its `base_data_path` attribute.
 
     --layer is the base name of a shapefile and its associated DBF and other
     files. In this example, the pirate directory contains the files
     str06oct.shp, str06oct.dbf, etc.
 
-    -n (short for --no-prompt) indicates that we want to run all of the data
-    integration actions without being prompted. In normal use, this will
-    probably be the default.
+    --no-prompt indicates that we want to run all of the data integration
+    actions without being prompted. In normal use, this will probably be the
+    default.
 
 Options::
 
-    --region | -r <region key>
-        The unique "region key" for a region. It should match the region's
-        Python module name. REQUIRED.
+    --region | -r REGION
+        The unique key for a region. It should match the region's Python
+        module name. REQUIRED.
 
-    --source | -d <data source name>
+    --source | -d SOURCE
         The data source. This is a directory containing shapefiles. For now,
         it is always relative to ${HOME}/byCycleData. REQUIRED.
 
-    --layer | -l <layer name>
-        The data layer. This is the base name for a shapefile and its related
-        files. For example, if the layer name is "str06oct", there will be
-        corresponding files named "str06oct.shp", "str06oct.dbf", etc.
+    --layer | -l LAYER
+        The data layer. This is the base name for a shapefile and its
+        related files. For example, if the layer name is "str06oct", there
+        will be corresponding files named str06oct.shp, str06oct.dbf, etc.
         REQUIRED.
 
-    --start | -s <index>
+    --start | -s START
         The index (0-based) of the action to start from. Yes, this is clunky
-        in that we need to know the index number for an action--a menu system
-        should be created. Defaults to 0.
+        in that we need to know the index number for an action--a menu
+        system should be created. Defaults to 0.
 
-    --end | -e <index>
+    --end | -e END
         Like --start but for the last action that should be run. Defaults to
         one less than the number of actions.
+
+    --only | -o INDEX
+        Do just the action indicated by INDEX, without prompting. --start
+        and --end should not be specified if this is. Implies --no-prompt.
 
     --no-prompt | -n
         Run the actions from --start to --end without prompting. The default
         is to prompt for every action.
 
-    --only | -o <index>
-        Do just the action indicated by ``index``, without prompting. By
-        default, this isn't used.
-
-    If no args are supplied, the default is to prompt for all actions.
-
-    If either of --start or --end is present, the -only option must not be
-    present.
-
-    If --only is present, --no-prompt is implied.
+    If none of --start, --end, or --only are specified, the default is to
+    prompt for all actions.
 
 """
-import sys, getopt
+import argparse
 
 
-def main(argv):
-    # Get command line options
-    opts = getOpts(sys.argv[1:])
+help_text = __doc__
 
-    region = opts.pop('region')
-    source = opts.pop('source')
-    layer = opts.pop('layer')
-    no_prompt = bool(opts.get('no_prompt', opts.get('only', False)))
 
-    imp_path = 'bycycle.core.model.%s.data.integrator' % region
+def main(argv=None):
+    parser = argparse.ArgumentParser(
+        description=help_text,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    parser.add_argument('-r', '--region', required=True)
+    parser.add_argument('-d', '--source', required=True)
+    parser.add_argument('-l', '--layer', required=True)
+    parser.add_argument('-n', '--no-prompt', action='store_true', default=False)
+    parser.add_argument('-s', '--start', type=int, default=None)
+    parser.add_argument('-e', '--end', type=int, default=None)
+    parser.add_argument('-o', '--only', type=int, default=None)
+
+    if argv is not None:
+        args = parser.parse_args(argv)
+    else:
+        args = parser.parse_args()
+
+    args.no_prompt = args.no_prompt or (args.only is not None)
+
+    start, end, only = args.start, args.end, args.only
+    if None in (start, end) and only is not None:
+        raise parser.error(
+            '--only not allowed with --start or --end')
+
+    imp_path = 'bycycle.core.model.{0.region}.data.integrator'.format(args)
     integrator_module = __import__(imp_path, locals(), globals(), [''])
 
-    integrator = integrator_module.Integrator(region, source, layer, **opts)
+    integrator = integrator_module.Integrator(
+        args.region, args.source, args.layer, args.no_prompt)
 
-    if opts['end'] is None:
-        opts['end'] = len(integrator.actions) - 1
-
-    integrator.run(**opts)
-
-
-def getOpts(argv):
-    """Parse the opts from ``argv`` and return them as a ``dict``."""
-    required_opts = 'region', 'source', 'layer'
-    opts = {
-        'start': 0,
-        'end': None,
-        'no_prompt': False,
-        'only': None,
-        }
-    # Parse args
-    try:
-        short_opts = 'r:d:l:s:e:no:h'
-        long_opts = ['region=', 'source=', 'layer=', 'start=', 'end=',
-                     'no-prompt', 'only=', 'help']
-        cl_opts, args = getopt.gnu_getopt(argv, short_opts, long_opts)
-    except getopt.GetoptError, e:
-        usage()
-        die(2, str(e))
-    start_or_end_specified = False
-    # See what args were given and put them in the args dict
-    for opt, val in cl_opts:
-        if opt not in ('--region', '-r',
-                       '--source', '-d',
-                       '--layer', '-l',
-                       '--no-prompt', '-n',
-                       '--help', '-h'):
-            try:
-                val = int(val)
-            except ValueError:
-                die(2, '%s value must be an integer.' % opt)
-        if opt in ('--region', '-r'):
-            opts['region'] = val
-        elif opt in ('--source', '-d'):
-            opts['source'] = val
-        elif opt in ('--layer', '-l'):
-            opts['layer'] = val
-        elif opt in ('--start', '-s'):
-            start_or_end_specified = True
-            opts['start'] = val
-        elif opt in ('--end', '-e'):
-            start_or_end_specified = True
-            opts['end'] = val
-        elif opt in ('--no-prompt', '-n'):
-            opts['no_prompt'] = True
-        elif opt in ('--only', '-o'):
-            opts['only'] = val
-        elif opt in ('--help', '-h'):
-            usage()
-            sys.exit()
-        else:
-            usage()
-            die(1, 'Unknown option: ``%s``' % opt)
-
-    required_missing = [o for o in required_opts if o not in opts]
-    if required_missing:
-        msg = 'Required options missing: %s' % ', '.join(required_missing)
-        usage()
-        die(2, msg)
-
-    if opts['only'] is not None:
-        if start_or_end_specified:
-            usage()
-            die(3, '--only must be the *only* argument or not specified.')
-        else:
-            opts['no_prompt'] = True
-
-    return opts
-
-def die(code=1, msg=''):
-    print 'ERROR: %s' % msg
-    sys.exit(code)
-
-def usage(msg=''):
-    if msg:
-        print '\n%s' % msg
-    print __doc__
-
-if __name__ == '__main__':
-    main(sys.argv)
+    integrator.run(
+        start=args.start, end=args.end, only=args.only,
+        no_prompt=args.no_prompt)
