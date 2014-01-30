a98b3beedd93f1da4916a41862b9441d2ccbb086
Wyatt Lee Baldwin <self@wyattbaldwin.com>
2014-01-30 09:52:11 -0800

    Remove useless TravelMode.__init__ method

--- bycycle/core/model/tmode.py
+++ bycycle/core/model/tmode.py
@@ -1,8 +1,5 @@
 class TravelMode(object):
 
-    def __init__(self):
-        pass
-
     def getEdgeWeight(self, v, edge_attrs, prev_edge_attrs):
         length = edge_attrs[self.indices['length']]
         return length
