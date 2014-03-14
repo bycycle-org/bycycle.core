cce9150c1ed9629ede55f8693ad5bc7abcf7e45b
Wyatt <self@wyattbaldwin.com>
2014-03-13 18:31:49 -0700

    Remove all unused functions from model.db
    - dropAllTables()
    - recreateTable()
    - deleteAllFromTable()
    - addColumn()
    - dropColumn()
    - getById()

--- bycycle/core/model/db.py
+++ bycycle/core/model/db.py
@@ -36,11 +36,6 @@ def make_url(drivername='postgresql', database='bycycle', **kwargs):
     return URL(drivername, database=database, **kwargs)
 
 
-def dropAllTables(md=None):
-    md = md or metadata
-    md.drop_all()
-
-
 def createAllTables(md=None):
     md = md or metadata
     md.create_all()
@@ -106,73 +101,6 @@ def dropTable(table, cascade=False):
             rollback()
         else:
             raise e
-
-
-def recreateTable(table):
-    """Drop ``table`` from database and then create it."""
-    dropTable(table)
-    table.create()
-
-
-def deleteAllFromTable(table):
-    """Delete all records from ``table``."""
-    try:
-        table.delete().execute()
-    except (psycopg2.ProgrammingError, SQLAlchemyError) as e:
-        if 'does not exist' in str(e):
-            rollback()
-        else:
-            raise e
-
-
-def addColumn(table_name, column_name, column_type):
-    try:
-        execute(
-            'ALTER TABLE %s ADD COLUMN %s %s' %
-            (table_name, column_name, column_type))
-    except psycopg2.ProgrammingErroras as e:
-        if 'already exists' not in str(e):
-            raise e
-    else:
-        commit()
-
-
-def dropColumn(table_name, column_name):
-    try:
-        execute('ALTER TABLE %s DROP COLUMN %s' % (table_name, column_name))
-    except psycopg2.ProgrammingErroras as e:
-        if 'already exists' not in str(e):
-            raise e
-    else:
-        commit()
-
-
-def getById(class_or_mapper, session, *ids):
-    """Get objects and order by ``ids``.
-
-    ``class_or_mapper`` Entity class or DB to object mapper
-    ``session`` DB session
-    ``ids`` One or more row IDs
-
-    return `list`
-      A list of domain objects corresponding to the IDs passed via ``ids``.
-      Any ID in ``ids`` that doesn't correspond to a row in ``table`` will
-      be ignored (for now), so the list may not contain the same number of
-      objects as len(ids). If ``ids`` is empty, an empty list is returned.
-
-    """
-    query = session.query(class_or_mapper)
-    objects = query.select(class_or_mapper.c.id.in_(*ids))
-    objects_by_id = dict(zip([object.id for object in objects], objects))
-    ordered_objects = []
-    for i in ids:
-        try:
-            ordered_objects.append(objects_by_id[i])
-        except KeyError:
-            # No row with ID==i in ``table``
-            # TODO: Should we insert None instead or raise???
-            pass
-    return ordered_objects
 
 
 if __name__ == '__main__':
