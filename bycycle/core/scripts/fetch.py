4815045b2c9baeb8ff8ef6bd7ed9d14f2491b6f5
Wyatt <self@wyattbaldwin.com>
2014-05-14 19:00:59 -0700

    Add individual minx, miny, maxx, maxy args to fetch script
    Removed bbox arg.
    
    This is simpler, and it's self-documenting.

--- bycycle/core/scripts/fetch.py
+++ bycycle/core/scripts/fetch.py
@@ -1,22 +1,20 @@
 import argparse
 
-from tangled.converters import as_tuple_of
-
 from bycycle.core.osm import OSMDataFetcher
-
-
-as_tuple_of_float = as_tuple_of(float)
 
 
 def fetch(argv=None):
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        'bbox', type=as_tuple_of_float, help='BBOX as "minx miny maxx maxy"')
+    parser.add_argument('minx', type=float)
+    parser.add_argument('miny', type=float)
+    parser.add_argument('maxx', type=float)
+    parser.add_argument('maxy', type=float)
     parser.add_argument('file_name')
     parser.add_argument('--url')
     args = parser.parse_args(argv)
     kwargs = {}
     if args.url:
         kwargs['url'] = args.url
-    fetcher = OSMDataFetcher(args.bbox, args.file_name, **kwargs)
+    bbox = [args.minx, args.miny, args.maxx, args.maxy]
+    fetcher = OSMDataFetcher(bbox, args.file_name, **kwargs)
     fetcher.run()
