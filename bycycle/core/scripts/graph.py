a3c5df2a161ca144d08832c89986d96067d1ee74
Wyatt <self@wyattbaldwin.com>
2014-05-14 18:42:06 -0700

    Switch order of args in graph builder
    Putting the database URL before the output file name reads more naturally.

--- bycycle/core/scripts/graph.py
+++ bycycle/core/scripts/graph.py
@@ -5,8 +5,8 @@ from bycycle.core.osm import OSMGraphBuilder
 
 def make_graph(argv=None):
     parser = argparse.ArgumentParser()
-    parser.add_argument('file_name', help='Output file name')
     parser.add_argument('db_url')
+    parser.add_argument('file_name', help='Output file name')
     args = parser.parse_args(argv)
-    graph_builder = OSMGraphBuilder(args.file_name, args.db_url)
+    graph_builder = OSMGraphBuilder(args.db_url, args.file_name)
     graph_builder.run()
