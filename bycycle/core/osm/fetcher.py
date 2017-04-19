8f96705b1b2f1f099341e6127ea02744b2e20966
Wyatt Baldwin <wbaldwin@pdx.edu>
2017-04-19 12:44:38 -0700

    Improve OSMDataFetcher
    - Use default URL if passed URL is None
    - Show progress when downloading

--- bycycle/core/osm/fetcher.py
+++ bycycle/core/osm/fetcher.py
@@ -9,10 +9,21 @@ class OSMDataFetcher:
     """Fetch OSM data inside a bounding box."""
 
     def __init__(self, bbox, file_name, url=DEFAULT_URL):
+        if url is None:
+            url = DEFAULT_URL
         self.bbox = bbox
         bbox = ','.join(str(f) for f in bbox)
         self.url = url.format(bbox)
         self.file_name = file_name
 
     def run(self):
-        urlretrieve(self.url, self.file_name)
+        def hook(num_blocks, block_size, total_size):
+            size = num_blocks * block_size
+            if total_size == -1:
+                msg = f'\r{size}B'
+            else:
+                msg = f'\r{size}B of {total_size}B'
+            print(msg, end='', flush=True)
+        print(f'Fetching {self.url}...', flush=True)
+        urlretrieve(self.url, self.file_name, hook)
+        print(f'\nSaved to {self.file_name}')
