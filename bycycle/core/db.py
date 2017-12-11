fc8c4bd6dcb97b33c7cd98e48ab879adc4f50146
Wyatt Baldwin <self@wyattbaldwin.com>
2017-12-11 13:32:29 -0800

    Don't re-create global SQLAlchemy engine if db.init() is called again

--- bycycle/core/db.py
+++ bycycle/core/db.py
@@ -14,8 +14,9 @@ session_factory = None
 
 def init(**connection_args):
     global engine, session_factory
-    engine = create_engine(make_url(**connection_args))
-    session_factory = sessionmaker(bind=engine)
+    if engine is None:
+        engine = create_engine(make_url(**connection_args))
+        session_factory = sessionmaker(bind=engine)
 
 
 def make_session():
