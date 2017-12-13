0b7cf9a6800bb980e9c065d28139efdd6814e7b8
Wyatt Baldwin <self@wyattbaldwin.com>
2017-12-13 14:14:33 -0800

    Add basic tests for lookup service

--- bycycle/core/tests/test_service/test_lookup.py
+++ bycycle/core/tests/test_service/test_lookup.py
@@ -1,9 +1,29 @@
 import unittest
 
+from bycycle.core import db
 from bycycle.core.model import LookupResult
 from bycycle.core.service import LookupService
 
 
 class TestLookupService(unittest.TestCase):
 
-    pass
+    def setUp(self):
+        self.engine, self.session_factory = db.init()
+        self.session = self.session_factory()
+
+    def tearDown(self):
+        self.session.close()
+
+    def _query(self, q, **kwargs):
+        service = LookupService(self.session)
+        return service.query(q, **kwargs)
+
+    def test_lookup_point(self):
+        result = self._query('-122.672655, 45.548242')
+        self.assertIsInstance(result, LookupResult)
+        self.assertEqual(result.name, 'N Fremont St')
+
+    def test_lookup_cross_streets(self):
+        result = self._query('NE 9th and Holladay')
+        self.assertIsInstance(result, LookupResult)
+        self.assertEqual(result.name, 'NE 9th Ave & NE Holladay St')
