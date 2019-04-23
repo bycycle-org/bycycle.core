fc8f87e80a819753629668fabae01a5611ce142a
Wyatt Baldwin <self@wyattbaldwin.com>
2019-04-23 15:27:54 -0700

    Allow config to be passed to services as constructor keyword args
    The intended use is to allow things like a location (e.g., current map
    center) and/or BBOX to be passed into services.
    
    For example, the `LookupService` could use this to only search near a
    given point or within certain bounds (and that's the original impetus
    for adding this).

--- bycycle/core/service/base.py
+++ bycycle/core/service/base.py
@@ -5,9 +5,16 @@ class AService(metaclass=ABCMeta):
 
     """Base class for byCycle services."""
 
-    def __init__(self, session):
-        """Initialize service with database ``session``."""
+    def __init__(self, session, **config):
+        """Initialize service.
+
+        Args:
+            session: SQLAlchemy database session
+            config: Additional service-specific configuration
+
+        """
         self.session = session
+        self.config = config
 
     @abstractmethod
     def query(self, q, **kwargs):
