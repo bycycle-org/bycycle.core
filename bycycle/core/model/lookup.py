05c45b630562071c5a2acee1fea1b92f6aafa2a4
Wyatt Baldwin <self@wyattbaldwin.com>
2021-10-07 19:54:01 -0700

    Add optional, source-specific data attribute to LookupResult model
    Initially, this is so the Mapbox `relevance` and prominence `score`
    attributes can be included.

--- bycycle/core/model/lookup.py
+++ bycycle/core/model/lookup.py
@@ -29,11 +29,13 @@ class LookupResult(Entity):
             result; defaults to location as ``'{latitude}, {longitude}``
         attribution (str):
             Attribution for the result (i.e., it's source; e.g., Mapbox)
+        data (dict):
+            Additional, source-specific data
 
     """
 
     def __init__(self, original_input, normalized_input, geom, closest_object, name,
-                 attribution=None):
+                 attribution=None, data=None):
         self.id = f'{closest_object.__class__.__name__}:{closest_object.id}'.lower()
         self.original_input = original_input
         self.normalized_input = normalized_input
@@ -41,6 +43,7 @@ class LookupResult(Entity):
         self.closest_object = closest_object
         self.name = name or f'{geom.y:.5f}, {geom.x:.5f}'
         self.attribution = attribution
+        self.data = data
 
     def __str__(self):
         return '\n'.join(str(attr) for attr in (self.name, self.geom))
